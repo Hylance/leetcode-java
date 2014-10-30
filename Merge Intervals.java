aca04a007ac72e0be1c5d7d9fcaab0209907f543
shuhengguo <shuhengguo@xiaoshuwangguodeMacBook-Air.local>
2014-10-30 00:19:10 -0400

    new

---
+++ Merge Intervals.java
@@ -0,0 +1,28 @@
+/*Merge Intervals:Given a collection of intervals, merge all overlapping intervals. For example, Given [1,3],[2,6],[8,10],[15,18], return [1,6],[8,10],[15,18].*/
+public class Solution {
+	public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+		if (intervals == null || intervals.size() <= 1) {
+			return intervals;
+		}
+		Collections.sort(intervals, new IntervalComparator());
+		ArrayList<Interval> result = new ArrayList<Interval>();
+		Interval last = intervals.get(0);
+		for (int i = 1; i < intervals.size(); i++) {
+			Interval curt = intervals.get(i);
+			if (curt.start <= last.end) {
+				last.end = Math.max(last.end, curt.end);
+			}
+			else {
+				result.add(last);
+				last = curt;
+			}
+		}
+		result.add(last);
+		return result;
+	}
+	private class IntervalComparator implements Comparator<Interval> {
+		public int compare(Interval a, Interval b) {
+			return a.start - b.start;
+		}
+	}
+}
